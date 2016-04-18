c66d0a8115245315cca760a1fb50ba14de6c83f7
Treho <hanshoffman@users.noreply.github.com>
2016-04-18 10:23:37 -0400

    Create bitsy.c

---
+++ bitsy.c
@@ -0,0 +1,30 @@
+#include <stdio.h> 
+#include <stdlib.h> 
+ 
+#define MAX_NUM 1000000 
+int LOOK_UP[MAX_NUM]; 
+ 
+boolean isFibonacci(int n) {
+//return true is fib
+//is it faster to just store fibs up to MAX_NUM then reference or recompute every single time?
+}
+
+boolean isPalindrome(int n) {
+
+}
+
+boolean isPrime(int n) {
+
+}
+
+boolean multOf3(int n) {
+
+}
+
+boolean multOf5(int n) {
+
+}
+
+int main(int argc, const char* argv[]) { 
+
+}
