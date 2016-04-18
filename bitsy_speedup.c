7411fbf944cdd563a00fc3b2dc80ff8a2a6b47ca
Hans Hoffman <hdot@GreenCouch.fios-router.home>
2016-04-18 17:37:27 -0400

    ready for speedup attempt

--- bitsy_speedup.c
+++ bitsy_speedup.c
@@ -1,29 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
-//#include <omp.h>
+#include <omp.h>
 
 //#define MAX_NUM 1000000
 #define MAX_NUM 100
 
-int isPerfectSquare(double n) {
-    int s = (int)sqrt(n);
-    return if s*s == n ? 1 : 0;
-}
-
 //http://www.geeksforgeeks.org/check-number-fibonacci-number/
 //https://www.quora.com/What-is-the-most-efficient-algorithm-to-check-if-a-number-is-a-Fibonacci-Number
+int isPerfectSquare(double n) {
+    int s = (int)sqrt(n);
+    return s*s == n ? 1 : 0;
+}
 int isFibonacci(int n) { return isPerfectSquare(5*n*n + 4) || isPerfectSquare(5*n*n - 4); }
 
 //http://stackoverflow.com/questions/199184/how-do-i-check-if-a-number-is-a-palindrome
 int isPalindrome(int n) {
-    int rev = 0, digit = 0;
-    while (n > 0) {
-        digit = num % 10;
+    int rev = 0, digit = 0, _n = n;
+    while (_n > 0 && n > 10) {
+        digit = _n % 10;
         rev = rev * 10 + digit;
-        num = num / 10;
+        _n = _n / 10;
     }
-    return if (n == rev ? 1 : 0);
+    return rev == n ? 1 : 0;
 }
 
 //https://en.wikipedia.org/wiki/Primality_test
@@ -32,21 +31,24 @@ int isPrime(int n) {
     if (n <= 3) return 1;
     if (n % 2 == 0 || n % 3 == 0) return 0;
     
-    int i = 5;
-    while (i * i <= n) {
-        if (n % i == 0 || n % (i+2) == 0) return 0;
-        i += 6;
+    for (int i = 5; i*i <= n; i+=6) {
+	if (n % i == 0 || n % (i+2) == 0) return 0;
     }
+    return 1;
 }
 
 int main(int argc, const char* argv[]) { 
     //http://bisqwit.iki.fi/story/howto/openmp/
+    #pragma omp parallel for
     //#pragma omp parallel for private(i) schedule(dynamic)
-    for (int i = 0, j = 0; i <= MAX_NUM; i++) {
-        if (isFibonacci(i)) printf("Fibbits ");
+    int j = 0;
+    for (int i = 1; i <= MAX_NUM; i++) {
+        printf("\n%d -> ", i);
+	if (isFibonacci(i)) printf("Fibbits ");
         if (isPalindrome(i)) printf("BitstiB ");
         if (isPrime(i)) printf("BitsForEveryone! ");
-        if (if i % 3 == 0 ? 1 : 0) { printf("CrowdOfBits "); j+=i; }
-        if (if n % 5 == 0 ? 1 : 0) { printf("MuchBits "); j+=i; }
-    } printf("\n%d", j);
+        if (i % 3 == 0 ? 1 : 0) { printf("CrowdOfBits "); j+=i; }
+        if (i % 5 == 0 ? 1 : 0) { printf("MuchBits "); j+=i; }
+    } 
+    printf("\n%d\n", j);
 }
